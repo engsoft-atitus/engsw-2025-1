e4e256361a13964a80eb4b2dcc64405f6272215a
Arthur Atitus <1137370@atitus.edu.br>
2025-05-06 21:01:08 -0300

    Upload de imagens

--- comunidade/models.py
+++ comunidade/models.py
@@ -6,8 +6,9 @@ from random import randint
 # Classe herda da class Model
 class Community(models.Model):
     nome = models.CharField(max_length=100,validators=[MinLengthValidator(5)])
-    nome_tag = models.CharField(max_length=104,unique=True,validators=[MinLengthValidator(9)])
+    nome_tag = models.CharField(max_length=105,unique=True,validators=[MinLengthValidator(10)])
     sobre = models.CharField(max_length=256)
+    profile_picture = models.ImageField(upload_to='uploads/')
 
     def nome_tag_generator(self):
         while True:
