f34232b9751029619d42314ffd4b2ec971f36979
Arthur Suse <galaticghost@gmail.com>
2025-05-07 14:39:37 -0300

    Commit de emergencia

--- comunidade/models.py
+++ comunidade/models.py
@@ -8,7 +8,7 @@ class Community(models.Model):
     nome = models.CharField(max_length=100,validators=[MinLengthValidator(5)])
     nome_tag = models.CharField(max_length=105,unique=True,validators=[MinLengthValidator(10)])
     sobre = models.CharField(max_length=256)
-    profile_picture = models.ImageField(upload_to='uploads/')
+    profile_picture = models.ImageField(upload_to='comunidade/static/uploads/')
 
     def nome_tag_generator(self):
         while True:
