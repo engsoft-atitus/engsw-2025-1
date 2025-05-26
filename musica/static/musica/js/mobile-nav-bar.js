1facff359bfb063c5ab17598149bc4360a5ebf3a
EduFag <edufagundes2005@gmail.com>
2025-05-26 20:04:08 -0300

    arrumando problema da nav-bar

--- musica/static/musica/js/mobile-nav-bar.js
+++ musica/static/musica/js/mobile-nav-bar.js
@@ -1,20 +1,25 @@
-const mobileMenu = document.querySelector ('.mobile-menu');
-const NavList = document.querySelector ('.nav-list');
-const header = document.querySelector ('header');
-const main = document.querySelector ('main');
-const footer = document.querySelector ('footer');
-const closeButton = document.querySelector('.close-menu');
+const mobileMenus = document.querySelectorAll('.mobile-menu');
+const navLists = document.querySelectorAll('.nav-list');
+const closeButtons = document.querySelectorAll('.close-menu');
 
-mobileMenu.addEventListener('click', () => {
-    NavList.classList.toggle('active');
-    header.classList.toggle('active');
-    main.classList.toggle('none');
-    footer.classList.toggle('none');
+const header = document.querySelector('header');
+const main = document.querySelector('main');
+const footer = document.querySelector('footer');
+
+mobileMenus.forEach((mobileMenu, index) => {
+    mobileMenu.addEventListener('click', () => {
+        navLists[index].classList.toggle('active');
+        header.classList.toggle('active');
+        main.classList.toggle('none');
+        footer.classList.toggle('none');
+    });
 });
 
-closeButton.addEventListener('click', () => {
-    NavList.classList.remove('active');
-    header.classList.remove('active');
-    main.classList.remove('none');
-    footer.classList.remove('none');
+closeButtons.forEach((closeButton, index) => {
+    closeButton.addEventListener('click', () => {
+        navLists[index].classList.remove('active');
+        header.classList.remove('active');
+        main.classList.remove('none');
+        footer.classList.remove('none');
+    });
 });
