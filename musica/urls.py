8fb3f10993f8b6b7fba96e7c3e7f8c58df0198ca
JVRPer <joaovitorribeiro1110@gmail.com>
2025-06-02 16:47:22 -0300

    Removido a rota de logout provisório; Alterado o nome da view da rota da listagem das playlists;

--- musica/urls.py
+++ musica/urls.py
@@ -5,10 +5,9 @@ urlpatterns = [
     path('', views.buscar_musicas, name='buscar_musicas'),
     path('player/', views.player, name='player'),
     path('salvar/', views.salvar_musica, name='salvar_musica'),
-    path('playlists/', views.listar_playlists, name='listar_playlists'),
+    path('playlists/', views.listar_playlists_usuario, name='listar_playlists'),
     path('playlist/<int:playlist_id>/', views.ver_playlist, name='ver_playlist'),
     path('playlist/<int:playlist_id>/excluir/', views.excluir_playlist, name='excluir_playlist'),
     path('criar_playlist/', views.criar_playlist, name='criar_playlist'),
-    path('logout/', views.logout_view, name='logout'),
 
 ]
