9da495928927597dfa8afeb94532928bebc14928
JVRPer <joaovitorribeiro1110@gmail.com>
2025-06-03 23:50:18 -0300

    Alterado a lógica de visualização das playlists, agora é possível encontrar as playlists após pesquisar

--- musica/views.py
+++ musica/views.py
@@ -6,6 +6,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from .models import MusicaSalva, Playlist
 from django.contrib.auth import logout
+from django.contrib import messages
+from django.urls import reverse
 import json
 
 # Create your views here.
@@ -37,10 +39,11 @@ def pesquisa_musica(request):
         print("Musicas")
         return render(request, 'buscar.html', {'musicas': musicas_encontradas, 'playlists': playlists})
 
-    elif search_type == "playlists":
-
-        return redirect("listar_playlists_todos")
-    print("Teste3")
+    elif search_type == "playlists" and query:
+        return redirect(f"{reverse('listar_playlists_todos')}?q={query}") # O reverse é responsável por redirecionar para a URL usando como ela foi definida no urls.py
+    else:
+        if search_type != "musicas" and search_type != "playlists":
+            messages.error(request, "Por favor, selecione 'Músicas' ou 'Playlists'.")
     return render(request, 'buscar.html', {'musicas': [], 'playlists': playlists})
 
 @login_required
@@ -90,34 +93,32 @@ def salvar_musica(request):
 
 @login_required
 def ver_playlist(request, playlist_id):
-    playlist = get_object_or_404(Playlist, id=playlist_id, user=request.user)
-    if playlist.user != request.user:
-        return HttpResponse("Você não tem permissão para ver esta playlist.")
-    else:
-        musicas_salvas = playlist.musicas.all()  # Obtém todas as músicas salvas na playlist
-        musicas_encontradas = []
+    playlist = get_object_or_404(Playlist, id=playlist_id)
+    
+    musicas_salvas = playlist.musicas.all()  # Obtém todas as músicas salvas na playlist
+    musicas_encontradas = []
 
-        for musica in musicas_salvas:
-            url = f"https://api.deezer.com/search?q={musica.nome} {musica.artista}&limit=1"
-            r = requests.get(url)
-            dados = r.json()
-            if dados.get("data"):
-                track = dados["data"][0]
-                musica_info = {
-                    'nome': track['title'],
-                    'linkmusica': track['preview'],
-                    'nomeartista': track['artist']['name'],
-                    'imagem': track['album']['cover_medium'],
-                }
-                musicas_encontradas.append(musica_info)
-                print("JSON da música:", json.dumps(musica_info, indent=4))
+    for musica in musicas_salvas:
+        url = f"https://api.deezer.com/search?q={musica.nome} {musica.artista}&limit=1"
+        r = requests.get(url)
+        dados = r.json()
+        if dados.get("data"):
+            track = dados["data"][0]
+            musica_info = {
+                'nome': track['title'],
+                'linkmusica': track['preview'],
+                'nomeartista': track['artist']['name'],
+                'imagem': track['album']['cover_medium'],
+            }
+            musicas_encontradas.append(musica_info)
+            print("JSON da música:", json.dumps(musica_info, indent=4))
 
         request.session['musicas'] = musicas_encontradas
 
-        return render(request, 'playlist.html', {
-            'musicas': musicas_encontradas,
-            'playlist': playlist
-        })
+    return render(request, 'playlist.html', {
+        'musicas': musicas_encontradas,
+        'playlist': playlist
+    })
         
 @login_required
 def criar_playlist(request):
@@ -147,5 +148,9 @@ def listar_playlists_usuario(request):
 
 @login_required
 def listar_playlists_todos(request):
-    playlists = Playlist.objects.all()
-    return render(request, 'minhasPlaylists.html', {'playlists': playlists})
+    query = request.GET.get("q")
+    if query:
+        playlists = Playlist.objects.filter(nome__icontains=query)
+    else:
+        playlists = Playlist.objects.all()
+    return render(request, 'playlistsTodos.html', {'playlists': playlists})
