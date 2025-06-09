90d9424f4b975724ad86423b10133ffd9d9c980a
EduFag <edufagundes2005@gmail.com>
2025-06-09 00:08:43 -0300

    ajustando front

---
+++ musica/static/musica/js/filtragem-pesquisa.js
@@ -0,0 +1,45 @@
+const searchTypeInput = document.getElementById("tipo-pesquisa");
+const musicButton = document.querySelector(".option-button:nth-child(1)");
+const playlistButton = document.querySelector(".option-button:nth-child(2)");
+const usuariosButton = document.querySelector(".option-button:nth-child(3)");
+const comunidadesButton = document.querySelector(".option-button:nth-child(4)");
+
+searchTypeInput.value = "musicas";
+
+searchTypeInput.value = "playlists";
+
+searchTypeInput.value = "usuarios";
+
+searchTypeInput.value = "comunidades";
+
+musicButton.addEventListener("click", function () {
+    searchTypeInput.value = "musicas";
+    musicButton.style.border = "1px solid #6545F0";
+    playlistButton.style.border = "none";
+    usuariosButton.style.border = "none";
+    comunidadesButton.style.border = "none";
+});
+
+playlistButton.addEventListener("click", function () {
+    searchTypeInput.value = "playlists";
+    playlistButton.style.border = "1px solid #6545F0";
+    musicButton.style.border = "none";
+    usuariosButton.style.border = "none";
+    comunidadesButton.style.border = "none";
+});
+
+usuariosButton.addEventListener("click", function () {
+    searchTypeInput.value = "usuarios";
+    usuariosButton.style.border = "1px solid #6545F0";
+    musicButton.style.border = "none";
+    playlistButton.style.border = "none";
+    comunidadesButton.style.border = "none";
+});
+
+comunidadesButton.addEventListener("click", function () {
+    searchTypeInput.value = "comunidades";
+    comunidadesButton.style.border = "1px solid #6545F0";
+    musicButton.style.border = "none";
+    playlistButton.style.border = "none";
+    usuariosButton.style.border = "none";
+});
