cf17c1a79790bab9561917a31790b32be6640454
JVRPer <joaovitorribeiro1110@gmail.com>
2025-06-10 17:22:56 -0300

    Adicionado a linha para imagens no form de Playlist

--- musica/forms.py
+++ musica/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Playlist
 
 class PlaylistForm(forms.ModelForm):
+    imagem = forms.ImageField(required=False)
     class Meta:
         model = Playlist
         fields = ['nome', 'descricao']
