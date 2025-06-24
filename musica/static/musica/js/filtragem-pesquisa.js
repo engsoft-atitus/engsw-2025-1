75805f1e2a49388466c3a088a1c28ececf18a798
EduFag <edufagundes2005@gmail.com>
2025-06-24 13:44:42 -0300

    front-end pronto (não entra nas plylists ainda, joão vai arrumar. Coloquei a home na barra de navegação)

--- musica/static/musica/js/filtragem-pesquisa.js
+++ musica/static/musica/js/filtragem-pesquisa.js
@@ -12,9 +12,15 @@ searchTypeInput.value = "usuarios";
 
 searchTypeInput.value = "comunidades";
 
+searchTypeInput.value = "musicas";
+musicButton.style.border = "5px solid #6545F0";
+playlistButton.style.border = "none";
+usuariosButton.style.border = "none";
+comunidadesButton.style.border = "none";
+
 musicButton.addEventListener("click", function () {
     searchTypeInput.value = "musicas";
-    musicButton.style.border = "1px solid #6545F0";
+    musicButton.style.border = "5px solid #6545F0";
     playlistButton.style.border = "none";
     usuariosButton.style.border = "none";
     comunidadesButton.style.border = "none";
@@ -22,15 +28,15 @@ musicButton.addEventListener("click", function () {
 
 playlistButton.addEventListener("click", function () {
     searchTypeInput.value = "playlists";
-    playlistButton.style.border = "1px solid #6545F0";
+    playlistButton.style.border = "5px solid #6545F0";
     musicButton.style.border = "none";
-    usuariosButton.style.border = "none";
+    usuariosButton.style.border = "none";   
     comunidadesButton.style.border = "none";
 });
 
 usuariosButton.addEventListener("click", function () {
     searchTypeInput.value = "usuarios";
-    usuariosButton.style.border = "1px solid #6545F0";
+    usuariosButton.style.border = "5px solid #6545F0";
     musicButton.style.border = "none";
     playlistButton.style.border = "none";
     comunidadesButton.style.border = "none";
@@ -38,7 +44,7 @@ usuariosButton.addEventListener("click", function () {
 
 comunidadesButton.addEventListener("click", function () {
     searchTypeInput.value = "comunidades";
-    comunidadesButton.style.border = "1px solid #6545F0";
+    comunidadesButton.style.border = "5px solid #6545F0";
     musicButton.style.border = "none";
     playlistButton.style.border = "none";
     usuariosButton.style.border = "none";
